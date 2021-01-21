e6c8109880b5f144add7fb0e4336a1d666ebc567
Moi Gutierrez <moyoteg@gmail.com>
2021-01-21 15:55:52 -0600

    added all files

---
+++ Sources/SwiftUIComponents/FilteredList.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Moi Gutierrez on 1/21/21.
+//
+
+import Foundation
---
+++ Sources/SwiftUIComponents/ViewModifier.swift
@@ -0,0 +1,42 @@
+//
+//  ViewModifier.swift
+//  PragMenu
+//
+//  Created by Moi Gutierrez on 1/21/21.
+//  Copyright © 2021 PragCore. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+public enum Modifier {
+    
+    struct ClearButton: ViewModifier {
+
+        @Binding var text: String
+
+        public init(text: Binding<String>) {
+            self._text = text
+        }
+
+        public func body(content: Content) -> some View {
+            HStack(alignment: .center) {
+                content
+
+                Spacer()
+                
+                if !text.isEmpty {
+                    Button(action: {
+                        withAnimation {
+                            self.text = ""
+                        }
+                    }) {
+                        Image(systemName: "delete.left")
+                            .foregroundColor(Color.blue)
+                    }
+                    .padding()
+                }
+            }
+        }
+    }
+}
